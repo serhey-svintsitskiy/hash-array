f7d2137a58499ebfcdfdb6babf8a4933d64c4793
serhey-svintsitskiy <sergeys08.sv@gmail.com>
2019-11-18 23:06:31 +0200

    bugfix with grater closest

--- test/HashArrayTest/TimestampHashArrayTest.php
+++ test/HashArrayTest/TimestampHashArrayTest.php
@@ -31,4 +31,28 @@ class TimestampHashArrayTest extends \PHPUnit\Framework\TestCase
         $this->assertEquals(20, $v2);
 
     }
+
+    public function testWithGraterClosest()
+    {
+        $hashArray = new HashArray\TimestampHashArray();
+
+        $t0 = time();
+        $t1 = $hashArray->set('a', 10);
+        sleep(3);
+
+        $t2 = $hashArray->set('a', 20);
+        sleep(5);
+
+        $t3 =  $hashArray->set('a', 30);
+
+        $v0 = $hashArray->get('a', $t0);
+        $v1 = $hashArray->get('a', $t1);
+        $v2 = $hashArray->get('a', $t2);
+        $v3 = $hashArray->get('a', $t3 - 2);
+
+        $this->assertEquals(10, $v1);
+        $this->assertEquals(20, $v2);
+        $this->assertEquals(20, $v3);
+
+    }
 }
