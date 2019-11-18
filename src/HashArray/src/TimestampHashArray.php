a08d56d86bc4361a918c2754d620a05978a574ab
Serhii Svintsitskyi <sergeys08.sv@gmail.com>
2019-11-18 19:47:03 +0200

    add binary search

--- src/HashArray/src/TimestampHashArray.php
+++ src/HashArray/src/TimestampHashArray.php
@@ -20,14 +20,15 @@ class TimestampHashArray implements TimestampHashArrayInterface
         if (!isset($this->storage[$key])) {
             throw new \RuntimeException("Could not find key '{$key}' in the HashArray");
         }
+//        $result = null;
+//        foreach ($this->storage[$key] as $savedTimestamp => $value) {
+//            if ($timestamp < $savedTimestamp) {
+//                break;
+//            }
+//            $result = $value;
+//        }
+        $result = $this->search($key, $timestamp);
 
-        $result = null;
-        foreach ($this->storage[$key] as $savedTimestamp => $value) {
-            if ($timestamp < $savedTimestamp) {
-                break;
-            }
-            $result = $value;
-        }
         return $result;
     }
 
@@ -41,5 +42,68 @@ class TimestampHashArray implements TimestampHashArrayInterface
         $timestamp = time();
         $this->storage[$key][$timestamp] = $value;
         return $timestamp;
+    }
+
+    protected function search($key, $timestamp)
+    {
+        $haystack = array_keys($this->storage[$key]);
+        $keyTs = $this->findClosest($haystack, count($haystack), $timestamp);
+        $value = $this->storage[$key][$keyTs] ?? null;
+        return $value;
+    }
+
+    function findClosest(array $arr, int $n, $target)
+    {
+        // Corner cases
+        if ($target < $arr[0])
+            return null;
+        if ($target == $arr[0])
+            return $arr[0];
+        if ($target >= $arr[$n - 1])
+            return $arr[$n - 1];
+
+        // Doing binary search
+        $i = 0;
+        $j = $n;
+        $mid = 0;
+        while ($i < $j) {
+            $mid = ($i + $j) / 2;
+            if ($arr[$mid] == $target)
+                return $arr[$mid];
+            /* If target is less than array element,
+                then search in left */
+            if ($target < $arr[$mid]) {
+
+                // If target is greater than previous
+                // to mid, return closest of two
+                if ($mid > 0 && $target > $arr[$mid - 1])
+                    return $this->getClosest($arr[$mid - 1],
+                        $arr[$mid], $target);
+
+                /* Repeat for left half */
+                $j = $mid;
+            } // If target is greater than mid
+            else {
+                if ($mid < $n - 1 &&
+                    $target < $arr[$mid + 1])
+                    return $this->getClosest($arr[$mid],
+                        $arr[$mid + 1], $target);
+                // update i
+                $i = $mid + 1;
+            }
+        }
+        // Only single element left after search
+        return $arr[$mid];
+    }
+
+    /**
+     * @param $val1
+     * @param $val2
+     * @param $target
+     * @return mixed
+     */
+    protected function getClosest($val1, $val2, $target)
+    {
+        return $target - $val1 >= $val2 - $target ? $val2 : $val1;
     }
 }
